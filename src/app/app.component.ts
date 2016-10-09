3727291ccdf5a7df0347951d5908079f0fdf65c1
Eugene <cuongnm265@gmail.com>
2016-10-09 15:48:54 +0700

    styling something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {CategoryService} from './news/category.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['app.component.scss'],
   providers: [CategoryService]
 })
 
